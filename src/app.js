96cdc130b30ab07e0aca8a4772b54574be1d9142
rishan3868vega@gmail.com <rishan3868vega@gmail.com>
2026-07-05 22:18:17 +0530

    feat(add):updated the postgres schema to add organisation and relations

--- src/app.js
+++ src/app.js
@@ -6,25 +6,31 @@ import morgan from "morgan";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 import authRouters from "./modules/auth/auth.router.js";
+import authMiddlewae from "./middlewares/authMiddleware.js";
 
-
-const app=express();
+const app = express();
 app.use(helmet());
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use(morgan("dev"));
 
-app.use("/api/auth",authRouters)
+app.use("/api/auth", authRouters);
 
-app.use(errorMiddleware)
+app.get("/api/protected", authMiddlewae, (req, res) => {
+  res.json({
+    success: true,
+    message: "access granted",
+    user: req.user,
+  });
+});
 
-app.get("/health",(req,res)=>{
-    res.status(200).json({
-        status:"OK"
-    })
-})
+app.use(errorMiddleware);
 
-
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+  });
+});
 
 export default app;
